8eca7bcfa056ed41eebdbffb72c01a7906464c55
Renan Hirayama <rhirayam@lxbk1131.gsi.de>
2025-01-31 16:57:22 +0100

    Add dynflu keys to base config and specific hydro config for dynflu

--- setup.sh
+++ setup.sh
@@ -11,9 +11,9 @@ mkdir -p ${configs_path}
 sed "s|HYBRID_PATH|${hybrid_path}|g" "submit.sh" > "${hybrid_path}/submit.sh"
 sed "s|HYBRID_PATH|${hybrid_path}|g" "submit_template.sh" > "${hybrid_path}/submit_template.sh"
 sed -e "s|CONFIG_PATH|${configs_path}|g" -e "s|SMASH_EXECUTABLE|${smash_exe}|g" "configs/IC_DynFlu_template.yaml" > "${configs_path}/IC_DynFlu_template.yaml"
-sed "s|VHLLE_EXECUTABLE|${vhlle_exe}|g" "configs/Hydro_DynFlu_template.yaml" > "${configs_path}/Hydro_DynFlu_template.yaml"
+sed -e "s|CONFIG_PATH|${configs_path}|g" -e "s|VHLLE_EXECUTABLE|${vhlle_exe}|g" "configs/Hydro_DynFlu_template.yaml" > "${configs_path}/Hydro_DynFlu_template.yaml"
 sed "s|SAMPLER_EXECUTABLE|${sampler_exe}|g" "configs/Sampler_DynFlu_template.yaml" > "${configs_path}/Sampler_DynFlu_template.yaml"
 sed "s|SMASH_EXECUTABLE|${smash_exe}|g" "configs/Afterburner_DynFlu_template.yaml" > "${configs_path}/Afterburner_DynFlu_template.yaml"
 
-cp configs/smash_initial_conditions_dynflu.yaml ${configs_path}/
+cp configs/{smash_initial_conditions_dynflu.yaml,vhlle_hydro_dynflu} ${configs_path}/
 cp create_hybrid_configs.sh dynamic_fluidization_run.py ${hybrid_path}
